9dce8eed46ac17d93d84cf7e4ea385aedc96d99b
Kubilay Cem KARAHAN <kubilaycem@yandex.com>
2024-01-08 10:18:44 +0300

    added-hangfirescript

---
+++ scripts/inithangfire.sql
@@ -0,0 +1,25 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 16.1 (Debian 16.1-1.pgdg120+1)
+-- Dumped by pg_dump version 16.1
+
+-- Started on 2024-01-07 12:27:48 UTC
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+
+CREATE DATABASE "HangfireDB" WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'en_US.utf8';
+
+
+ALTER DATABASE "CreditWiseHubDB" OWNER TO admin;
